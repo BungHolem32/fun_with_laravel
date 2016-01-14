9343ebc9f5dfccbc5ace5c4bde32e550b9da96e9
Carlosm <carlosm@rboptions.com>
2016-01-14 12:35:34 +0200

    css direction

--- resources/views/main/members/edit.blade.php
+++ resources/views/main/members/edit.blade.php
@@ -5,6 +5,8 @@
 @endsection
 
 @section('edit')
+    {{Request::local()->dir == '1' ? $dir = 'rtl' : $dir = 'ltr'}}
+
     @include('funnels.layouts._partials._page-layout-selection')
     <div class="form-group">
         {!! Form::label('formType', 'Chose Form Type:') !!}
@@ -16,25 +18,26 @@
 
     <div class="form-group">
         <h3><i class="fa fa-gg"></i> {!! Form::label('title_h1', 'Title:') !!}</h3>
-        {!! Form::text('title_h1', $page['title_h1'], ['name'=>'mongo[title_h1]', 'class'=>'form-control']) !!}
+        {!! Form::text('title_h1', $page['title_h1'], ['name'=>'mongo[title_h1]', 'class'=>$dir.' direction form-control']) !!}
     </div>
     <div class="form-group">
         <h3><i class="fa fa-newspaper-o"></i> {!! Form::label('description', 'Description:') !!}</h3>
-        {!! Form::text('description', $page['description'], ['name'=>'mongo[description]', 'class'=>'form-control']) !!}
+        {!! Form::text('description', $page['description'], ['name'=>'mongo[description]', 'class'=>$dir.' direction form-control']) !!}
     </div>
+
 
     <div class="form-group">
         <h3><i class="fa fa-video-camera"></i> {!! Form::label('video', 'Video link:') !!}</h3>
-        {!! Form::text('video', $page['video'], ['name'=>'mongo[video]', 'class'=>'form-control']) !!}
+        {!! Form::text('video', $page['video'], ['name'=>'mongo[video]', 'class'=>$dir.' direction form-control']) !!}
     </div>
 
     <div class="form-group">
         {!! Form::label('htmlHeader', 'Html Header:') !!}
-        {!! Form::textarea('htmlHeader', $page['htmlHeader'], ['name'=>'mongo[htmlHeader]', 'class'=>'editor form-control']) !!}
+        {!! Form::textarea('htmlHeader', $page['htmlHeader'], ['name'=>'mongo[htmlHeader]', 'class'=>$dir.' direction editor  form-control']) !!}
     </div>
     <div class="form-group">
         {!! Form::label('smallHeader', 'Below Header:') !!}
-        {!! Form::textarea('smallHeader', $page['smallHeader'], ['name'=>'mongo[smallHeader]', 'class'=>'editor form-control']) !!}
+        {!! Form::textarea('smallHeader', $page['smallHeader'], ['name'=>'mongo[smallHeader]', 'class'=>$dir.' direction editor form-control']) !!}
     </div>
 
 @endsection
@@ -44,15 +47,15 @@
         <h3><i class="fa fa-google"></i> Analytics & Scripts</h3>
         <div class="form-group">
             {!! Form::label('headScripts', 'before </head> Scripts:') !!}
-            {!! Form::textarea('headScripts', $page['scripts']['headScripts'], ['name'=>'mongo[scripts][headScripts]', 'class'=>'form-control']) !!}
+            {!! Form::textarea('headScripts', $page['scripts']['headScripts'], ['name'=>'mongo[scripts][headScripts]', 'class'=>$dir.' direction form-control']) !!}
         </div>
         <div class="form-group">
             {!! Form::label('afterBodyScripts', 'After <body> Scripts:') !!}
-            {!! Form::textarea('afterBodyScripts', $page['scripts']['afterBodyScripts'], ['name'=>'mongo[scripts][afterBodyScripts]', 'class'=>'form-control']) !!}
+            {!! Form::textarea('afterBodyScripts', $page['scripts']['afterBodyScripts'], ['name'=>'mongo[scripts][afterBodyScripts]', 'class'=>$dir.' direction form-control']) !!}
         </div>
         <div class="form-group">
             {!! Form::label('bodyEndScripts', 'before </body> Scripts:') !!}
-            {!! Form::textarea('bodyEndScripts', $page['scripts']['bodyEndScripts'], ['name'=>'mongo[scripts][bodyEndScripts]', 'class'=>'form-control']) !!}
+            {!! Form::textarea('bodyEndScripts', $page['scripts']['bodyEndScripts'], ['name'=>'mongo[scripts][bodyEndScripts]', 'class'=>$dir.' direction form-control']) !!}
         </div>
         <div class="form-group">
             {!! Form::checkbox('mongo[switches][recaptcha]', $page['switches']['recaptcha']) !!}
