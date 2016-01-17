5b29520efdf382fc9f405f2131f30dc05ba97476
Carlosm <carlosm@rboptions.com>
2016-01-17 10:40:54 +0200

    general binary on progress

--- resources/views/funnels/layouts/_partials/_form-horizontal.blade.php
+++ resources/views/funnels/layouts/_partials/_form-horizontal.blade.php
@@ -20,46 +20,49 @@ foreach($form->membersFields->getChildren() as $field){
 
 ?>
 <ul class="errorMessages"></ul>
+
 {!! Form::open(['url' => url('postForm').'?'.$_SERVER["QUERY_STRING"],
                 'id'=>'form',
                 'name'=>'accountForm',
                 'target'=>'_self',
                 'method'=>'post',
                 'class'=>'form-horizontal']) !!}
+    <div class="fields">
+        <input type="hidden" name="parentPage" value="{{ $funnelId }}">
+        @forelse($hiddenFields as $hiddenField)
+            <input type="hidden" id="{{ $hiddenField[0] }}" name="{{ $hiddenField[0] }}" value="">
+        @empty
+        @endforelse
+            @foreach($fields as $field)
+                @if($field[0] == 'text' || $field[0] == 'email' || $field[0] == 'password')
+                    <div class="field">
+                        @if(isset($show_recaptcha))
+                            <div class="g-recaptcha" data-sitekey="6Ld39RMTAAAAALVGhMswy185zq0C2bmP-gydSrSI"></div>
+                        @endif
+                        {{--{!! Form::$field[0]($field[1], null, ['class'=>'form-control', 'placeholder'=>@ln($field[2]) !!}--}}
+                        <input value="{{Request::get($field[1])}}" {{!empty($_REQUEST[$field[1]]) ? 'disabled="disabled"' : ''}}  type="{{ $field[0] }}" id="{{ $field[1] }}" class="form-control" name="{{ $field[1] }}" required="required" minlength="2" placeholder="{{ \App\Languages::getTrans($field[2]) }}" data-cip-id="{{ $field[1] }}">
+                    </div>
+                @elseif($field[0] == 'phone')
+                    <div class="field phoneFields">
+                        <input type="phone" id="prefix" class="" name="prefix" required="required" data-cip-id="prefix" />
+                        <input type="{{ $field[0] }}" id="{{ $field[1] }}" class="" name="{{ $field[1] }}" required="required" placeholder="{{ \App\Languages::getTrans($field[2]) }}" data-cip-id="{{ $field[1] }}">
+                    </div>
 
-    <input type="hidden" name="parentPage" value="{{ $funnelId }}">
-    @forelse($hiddenFields as $hiddenField)
-        <input type="hidden" id="{{ $hiddenField[0] }}" name="{{ $hiddenField[0] }}" value="">
-    @empty
-    @endforelse
-
-    <div class="row">
-        @foreach($fields as $field)
-            @if($field[0] == 'text' || $field[0] == 'email' || $field[0] == 'password')
-                <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 field">
-                    {{--{!! Form::$field[0]($field[1], null, ['class'=>'form-control', 'placeholder'=>@ln($field[2]) !!}--}}
-                    <input value="{{Request::get($field[1])}}" {{!empty($_REQUEST[$field[1]]) ? 'disabled="disabled"' : ''}}  type="{{ $field[0] }}" id="{{ $field[1] }}" class="form-control" name="{{ $field[1] }}" required="required" minlength="2" placeholder="{{ \App\Languages::getTrans($field[2]) }}" data-cip-id="{{ $field[1] }}">
-                </div>
-            @elseif($field[0] == 'phone')
-                <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 field phoneFields">
-                    <input type="phone" id="prefix" class="col-md-3 col-sm-3 col-xs-2" name="prefix" required="required" data-cip-id="prefix" />
-                    <input type="{{ $field[0] }}" id="{{ $field[1] }}" class="col-md-8 col-sm-8 col-xs-9 col-xs-offset-1" name="{{ $field[1] }}" required="required" placeholder="{{ \App\Languages::getTrans($field[2]) }}" data-cip-id="{{ $field[1] }}">
-                </div>
-
-            @elseif($field[0] == 'country')
-                <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 field">
-                    <select name="Country" id="country" class="form-control" required="required" data-cip-id="{{ $field[1] }}">
-                        @include('funnels.layouts._partials._countries')
-                    </select>
-                </div>
-            @elseif($field[0] == 'submit')
-                <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 field">
-                    <input type="{{ $field[0] }}" id="{{ $field[1] }}" class="form-control col-md-10" name="{{ $field[1] }}" value="{{ \App\Languages::getTrans($field[2]) }}" data-cip-id="{{ $field[1] }}">
-                </div>
-            @endif
-        @endforeach
+                @elseif($field[0] == 'country')
+                    <div class="field">
+                        <select name="Country" id="country" class="form-control" required="required" data-cip-id="{{ $field[1] }}">
+                            @include('funnels.layouts._partials._countries')
+                        </select>
+                    </div>
+                @elseif($field[0] == 'submit')
+                    <div class="field">
+                        <input type="{{ $field[0] }}" id="{{ $field[1] }}" class="form-control" name="{{ $field[1] }}" value="{{ \App\Languages::getTrans($field[2]) }}" data-cip-id="{{ $field[1] }}">
+                    </div>
+                @endif
+            @endforeach
     </div>
 {!! Form::close() !!}
+
 
 
 @section('bottom-scripts')
