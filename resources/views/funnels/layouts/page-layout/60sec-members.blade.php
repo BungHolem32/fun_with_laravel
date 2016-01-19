f1ea54b716f320984347959b1ad4553c5467d13f
Carlosm <carlosm@rboptions.com>
2016-01-19 17:31:56 +0200

    60sec

---
+++ resources/views/funnels/layouts/page-layout/60sec-members.blade.php
@@ -0,0 +1,19 @@
+@section('head')
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
+    {!! $page->appendAsset(url('/css/60sec.css')) !!}
+@append
+
+@section('bottom-scripts')
+    @com('funnel_scripts')
+@append
+
+@section('page-layout')
+    {!! $page->htmlHeader !!}
+    <div class="head"></div>
+    <div class="bg">
+        <div class="form">
+            @include('funnels.layouts._partials._form-vertical', ['funnelId' => $page->id])
+        </div>
+    </div>
+    <div class="bottom"></div>
+@endsection
