d15282b80d90f65675cf0cd3c4210c9ea50a887c
Carlosm <carlosm@rboptions.com>
2016-01-19 11:07:34 +0200

    general binary fix and without gulp maps

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,6 @@
 var elixir = require('laravel-elixir');
 var fs = require('fs');
-
+elixir.config.sourcemaps = false;
 /*
  |--------------------------------------------------------------------------
  | Elixir Asset Management
@@ -12,13 +12,11 @@ var fs = require('fs');
  |
  */
 
-
-
 elixir(function(mix) {
 
     /*mix.less(['admin.less'], 'public_html/adminpanel/css/admin.css')
-        */mix.less(['base-funnel-load-ltr.less'], 'public_html/css/generalbinary-0.css')
-        .less(['base-funnel-load-rtl.less'], 'public_html/css/generalbinary-1.css');
+        */mix.less(['base-funnel-load-ltr.less'], 'public_html/css/binary-currencies-0.css')
+        .less(['base-funnel-load-rtl.less'], 'public_html/css/binary-currencies-1.css');
 
     //mix.less(['brand/pages/trade.less'], 'public/css/trade.css');
     //mix.less(['brand/pages/accounts.less'], 'public/css/accounts.css');
