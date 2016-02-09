97090a29237720052bb121149c1e1ecb58a130e1
Carlosm <carlosm@rboptions.com>
2016-02-09 14:59:57 +0200

    aussie black - without popups

--- public_html/js/aussie/panel/black/main.js
+++ public_html/js/aussie/panel/black/main.js
@@ -1,54 +1,37 @@
+// nav fix
 $(window).on('scroll',function(){
     if($('.collapse.in').length)
         $('.collapse.in').removeClass('collapse').removeClass('in');
 });
-
 $('*:not(.nav)').on('click',function(){
     if($('.collapse.in').length)
         $('.collapse.in').removeClass('collapse').removeClass('in');
 });
 
 $(document).ready(function(){
+
     $('.toggle').toggles({
-        drag: true, // allow dragging the toggle between positions
-        click: true, // allow clicking on the toggle
         text: {
-            on: 'ON', // text for the ON position
-            off: 'OFF' // and off
+            on: $('.toggle').data('on'), // text for the ON position
+            off: $('.toggle').data('off') // and off
         },
-        on: false, // is the toggle ON on init
-        animate: 250, // animation time (ms)
-        easing: 'swing', // animation transition easing function
-        checkbox: null, // the checkbox to toggle (for use in forms)
-        clicker: null, // element that can be clicked on to toggle. removes binding from the toggle itself (use nesting)
+        animate: 180, // animation time (ms)
         width: 110, // width used if not set in css
-        height: 50, // height if not set in css
-        type: 'compact' // if this is set to 'select' then the select style toggle will be used
+        height: 50 // height if not set in css
     });
 
-// detect toggle change
-    (function(){
-        var originalAddClassMethod = jQuery.fn.addClass;
-        jQuery.fn.addClass = function(){
-            var result = originalAddClassMethod.apply( this, arguments );
-            jQuery(this).trigger('cssChanged');
-            return result;
+    $('.toggle').on('toggle', function(e, active) {
+        if (active) {
+            console.log('Toggle ON!');
+        } else {
+            console.log('Toggle OFF!');
         }
-    })();
-    $(".toggle-on,.toggle-off").bind('cssChanged', function(){
-        setTimeout(function(){
-            if($(".toggle-on").attr('class') == 'toggle-on active')
-                console.log('on');
-            else
-                console.log('off');
-        },100);
-
     });
 
-// detect amount
     $('.amount').on('click',function(){
-       $('.amount.active').removeClass('active');
-        $(this).addClass('active');
+        var tthis = $(this);
+        tthis.parent().parent().find('.active').removeClass('active');
+        tthis.addClass('active');
         $('input[name=amount]').val($(this).data('start'));
     });
 });
--- resources/views/funnels/layouts/page-layout/panel-black.blade.php
+++ resources/views/funnels/layouts/page-layout/panel-black.blade.php
@@ -1,6 +1,5 @@
 @section('head')
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
-    <link rel="stylesheet" href="{!!  '/css/lp/'.$lpName.'.css?v='.time() !!}">
 
 @append
 
