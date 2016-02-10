a394d3359ee13775888c1cc6fbb4bab02d3c8ef1
Carlosm <carlosm@rboptions.com>
2016-02-10 13:42:05 +0200

    general,currencies,aussie-black panel

--- public_html/js/aussie/panel/black/main.js
+++ public_html/js/aussie/panel/black/main.js
@@ -20,11 +20,32 @@ $(document).ready(function(){
         height: 50 // height if not set in css
     });
 
+    // cool loading jquery
+    function loading(){
+        $('.loading').show();
+        setInterval(function(){
+            if(!$('.loading span').hasClass('d1'))
+                $('.loading span').removeClass('d4').addClass('d1').html('.');
+            else if(!$('.loading span').hasClass('d2'))
+                $('.loading span').addClass('d2').html('..');
+            else if(!$('.loading span').hasClass('d3'))
+                $('.loading span').addClass('d3').html('...');
+            else if(!$('.loading span').hasClass('d4'))
+                $('.loading span').removeClass('d1').removeClass('d2').removeClass('d3').addClass('d4').html('');
+        },1300);
+    }
+    loading();
+    // hide loading:          $('.loading').hide();
+
+
+
+    /******* Actions *******/
+
     $('.toggle').on('toggle', function(e, active) {
         if (active) {
-            console.log('Toggle ON!');
+            console.log('Toggle ON');
         } else {
-            console.log('Toggle OFF!');
+            console.log('Toggle OFF');
         }
     });
 
@@ -33,5 +54,11 @@ $(document).ready(function(){
         tthis.parent().parent().find('.active').removeClass('active');
         tthis.addClass('active');
         $('input[name=amount]').val($(this).data('start'));
+
+        console.log('amount clicked');
+    });
+
+    $('.dep_btn').on('click',function(){
+        console.log('deposit clicked');
     });
 });
--- resources/views/funnels/layouts/page-layout/demotradesystem-members.blade.php
+++ resources/views/funnels/layouts/page-layout/demotradesystem-members.blade.php
@@ -1,8 +1,10 @@
 @section('head')
     <script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.0.0-alpha/js/bootstrap.min.js"></script>
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
+    <script src="/js/lp/lp.js"></script>
     <link href='https://fonts.googleapis.com/css?family=Roboto+Condensed:400,700,300,300italic' rel='stylesheet' type='text/css'>
-    {!! $page->appendAsset(url('/css/lp/binary-currencies-'.Request::local()->dir.'.css')) !!}
+    <meta name="theme-color" content="#c6ab2b">
+    {!! $page->appendAsset(url('/css/lp/demotradesystem-'.Request::local()->dir.'.css')) !!}
 @append
 
 @section('bottom-scripts')
@@ -34,17 +36,17 @@
                 {!! $page->htmlHeader !!}
                 <div class="form">
                     {!! $page->smallHeader !!}
-                    @include('funnels.layouts._partials._form-horizontal', ['funnelId' => $page->id])
+                    @include('funnels.layouts._partials._form-horizontal', ['funnelId' => $page->id,'btn'=>'/img/lp/demotradesystem/m-button-'.Request::local()->dir.'.png'])
                 </div>
             </div>
         </div>
     </div>
     <div class="main">
-        <div class="container col col-xs-12 col-sm-8 pers">
+        <div class="container col col-xs-12 col-sm-7">
             <div class="row">
                 <div class="col-sm-6 col-xs-12">
                     <div class="per">
-                        <img class="icon reduce1" src="/img/lp/binary-currencies/ico-1.png"/>
+                        <div class="icon ico1"></div>
                         <div class="title">
                             The perfect account type for every trader</div>
                             <div class="text">
@@ -52,8 +54,8 @@
                         </div>
                     </div>
                     <div class="per">
-                        <img class="icon" src="/img/lp/binary-currencies/ico-2.png"/>
-                        <div class="title">Dedicated Account Managers and Customer Service available 24/7</div>
+                        <div class="icon ico2"></div>
+                        <div class="title">Valuable education tools</div>
                         <div class="text">
                             Every trader gets a huge packet of educational materials designed to enhance your trading skills. Our Binary options e-book presents a simple breakdown of trading basics, and our team of analysts sends out daily reviews covering important market events.
                         </div>
@@ -61,14 +63,14 @@
                 </div>
                 <div class="col-sm-6 col-xs-12">
                     <div class="per">
-                        <img class="icon" src="/img/lp/binary-currencies/ico-3.png"/>
+                        <div class="icon ico3"></div>
                         <div class="title">An amazing trading platform</div>
                         <div class="text">
                             Our Binary Options platform is easy to master. Our senior brokers walk you through trading step by step, and teach you how to pick winning trades on currencies, stocks, indices, and commodities, all while earning the highest payouts in the industry – up to 88%.
                         </div>
                     </div>
                     <div class="per">
-                        <img class="icon" src="/img/lp/binary-currencies/ico-4.png"/>
+                        <div class="icon ico4"></div>
                         <div class="title">Welcome Bonuses and Rewards </div>
                         <div class="text">
                             RBoptions traders always get more! You can make your deposit go farther when we match it with a 50% deposit bonus directly into your account. Our traders also qualify for high end Apple products like iPhones and iPads.
@@ -76,13 +78,12 @@
                     </div>
                 </div>
             </div>
-            <div class="row icons">
-                <button class="submit onsubmit"><span class="img" style="display:none;" data-url="" data-text="TRADE NOW"></span> <span>TRADE NOW</span></button>
-                <img src="/img/lp/binary-currencies/payment.png" alt="">
-            </div>
         </div>
     </div>
     <div class="footer">
+        {{ $btn = $page->submitValue  }}
+        <a class="submit onsubmit"><span class="img" style="display:none;" data-url="{{ $btn }}" data-text="{{ \App\Languages::getTrans($btn) }}"></span> <span class="h-sm">{{ \App\Languages::getTrans($btn) }}</span></a>
+        <img src="/img/lp/demotradesystem/Footer-icons.png" alt="">
         <div class="bg">
             <div class="text">
             Eligibility:<br>
@@ -93,5 +94,5 @@
 
 
 
-    <script src="/js/lp/binary-currencies.js"></script>
+
 @endsection
