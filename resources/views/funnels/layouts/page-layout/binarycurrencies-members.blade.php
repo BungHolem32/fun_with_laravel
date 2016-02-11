4a2610053a0e90d297cd80aad86754c7cb4c9941
Carlosm <carlosm@rboptions.com>
2016-02-11 14:30:20 +0200

    general,currencies,aussie-black panel

--- resources/views/funnels/layouts/page-layout/binarycurrencies-members.blade.php
+++ resources/views/funnels/layouts/page-layout/binarycurrencies-members.blade.php
@@ -2,6 +2,7 @@
     <script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.0.0-alpha/js/bootstrap.min.js"></script>
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
     <link href='https://fonts.googleapis.com/css?family=Roboto+Condensed:400,700,300,300italic' rel='stylesheet' type='text/css'>
+    <script src="/js/lp/lp.js"></script>
     {!! $page->appendAsset(url('/css/lp/binary-currencies-'.Request::local()->dir.'.css')) !!}
 @append
 
@@ -21,7 +22,7 @@
                 {!! $page->htmlHeader !!}
                 <div class="form">
                     {!! $page->smallHeader !!}
-                    @include('funnels.layouts._partials._form-horizontal', ['funnelId' => $page->id])
+                    @include('funnels.layouts._partials._form-horizontal', ['funnelId' => $page->id,'btn'=>'/img/lp/binary-currencies/m-button-'.Request::local()->dir.'.png'])
                 </div>
             </div>
         </div>
@@ -31,41 +32,42 @@
             <div class="row">
                 <div class="col-sm-6 col-xs-12">
                     <div class="per">
-                        <img class="icon reduce1" src="/img/lp/binary-currencies/ico-1.png"/>
+                        <div class="icon ico1"></div>
                         <div class="title">
-                            The perfect account type for every trader</div>
+                            Valuable Education Tools </div>
                             <div class="text">
-        From beginners to advanced traders, we offer accounts designed with your needs in mind. Choose the services that you need from an extensive list which includes dedicated brokers, trading signals, and money management.
+                                Every trader gets a huge packet of educational materials designed to enhance your trading skills. Our Binary options e-book presents a simple breakdown of trading basics, and our team of analysts sends out daily reviews covering important market events.
                         </div>
                     </div>
                     <div class="per">
-                        <img class="icon" src="/img/lp/binary-currencies/ico-2.png"/>
+                        <div class="icon ico2"></div>
                         <div class="title">Dedicated Account Managers and Customer Service available 24/7</div>
                         <div class="text">
-                            Every trader gets a huge packet of educational materials designed to enhance your trading skills. Our Binary options e-book presents a simple breakdown of trading basics, and our team of analysts sends out daily reviews covering important market events.
+                            RBoptions is always here to help, with a team of experienced professionals trained to answer your questions. Feel free to contact us at any time, day or night, for inquiries about trading or your account. We are devoted to your success.offering the perfect training environment.
                         </div>
                     </div>
                 </div>
                 <div class="col-sm-6 col-xs-12">
                     <div class="per">
-                        <img class="icon" src="/img/lp/binary-currencies/ico-3.png"/>
-                        <div class="title">An amazing trading platform</div>
+                        <div class="icon ico3"></div>
+                        <div class="title">Welcome Bonuses And Rewards </div>
                         <div class="text">
-                            Our Binary Options platform is easy to master. Our senior brokers walk you through trading step by step, and teach you how to pick winning trades on currencies, stocks, indices, and commodities, all while earning the highest payouts in the industry – up to 88%.
+                            RBoptions traders always get more! You can make your deposit go farther when we match it with a 50% deposit bonus directly into your account. Our traders also qualify for high end Apple products like iPhones and iPads.
                         </div>
                     </div>
                     <div class="per">
-                        <img class="icon" src="/img/lp/binary-currencies/ico-4.png"/>
-                        <div class="title">Welcome Bonuses and Rewards </div>
+                        <div class="icon ico4"></div>
+                        <div class="title">$50,000 Demo Account </div>
                         <div class="text">
-                            RBoptions traders always get more! You can make your deposit go farther when we match it with a 50% deposit bonus directly into your account. Our traders also qualify for high end Apple products like iPhones and iPads.
+                            Every new binary options trading account gets access to a $50,000 demo where you can try out strategies and techniques risk-free. The demo account offers the same range of assets as your live trading account, offering the perfect training environment.
                         </div>
                     </div>
                 </div>
             </div>
             <div class="row icons">
                 <button class="submit onsubmit"><span class="img" style="display:none;" data-url="" data-text="TRADE NOW"></span> <span>TRADE NOW</span></button>
-                <img src="/img/lp/binary-currencies/payment.png" alt="">
+                <img class="sm-h" src="/img/lp/binary-currencies/payment.png" alt="">
+                <img class="sm-s" src="/img/lp/binary-currencies/m-payments.png" alt="">
             </div>
         </div>
     </div>
