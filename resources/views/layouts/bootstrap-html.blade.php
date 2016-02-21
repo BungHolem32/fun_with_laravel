f99de3b2d03dd8d68e2541ec6be3b96462fd21bc
rotemg <rotemg@rboptions.com>
2016-02-21 15:44:36 -0100

    rboptions-org - css / less / and html

--- resources/views/layouts/bootstrap-html.blade.php
+++ resources/views/layouts/bootstrap-html.blade.php
@@ -4,14 +4,7 @@
 <head>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
-    <meta name="description" content="">
-
-    <title>Carousel Template for Bootstrap</title>
-
-    <!-- Bootstrap core CSS -->
-    <link href="/css/bootstrap/rboptions-org-{{ Request::local()->dir }}.css" rel="stylesheet">
+    @yield('head')
 </head>
 <!-- NAVBAR
 ================================================== -->
@@ -25,5 +18,6 @@
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
 <script>window.jQuery || document.write('<script src="../../assets/js/vendor/jquery.min.js"><\/script>')</script>
 <script src="/js/vendor/bootstrap/bootstrap.min.js"></script>
+@yield('bottom-scripts')
 </body>
 </html>
--- resources/views/main/homearticle/index.blade.php
+++ resources/views/main/homearticle/index.blade.php
@@ -1,5 +1,14 @@
 @extends('layouts.bootstrap-html')
 
+@section('head')
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <title>Carousel Template for Bootstrap</title>
+    <!-- Bootstrap core CSS -->
+    <link href="/css/bootstrap/rboptions-org-{{ Request::local()->dir }}.css" rel="stylesheet">
+    <link rel="stylesheet" href="/js/vendor/flickity/flickity.css" />
+    <script src="/js/vendor/flickity/flickity.pkgd.js"></script>
+@append
 
 @section('content')
     <div class="container">
@@ -37,53 +46,91 @@
                 <h1 class="capitalize">In <span class="big">Binary Options</span>,<br>&nbsp;&nbsp; &nbsp;There's Only <span class="big">One Perfect Choice</span></h1>
             </div>
         </div>
-    </div>
 
-    <div class="wrapper bg-white">
-        <div class="container marketing center">
-            <!-- Three columns of text below the carousel -->
-            <div class="row center">
-                <div class="col-xs-12 below-header">
-                    <p>Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit. Cras mattis consectetur purus sit amet fermentum. Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit. Cras mattis consectetur purus sit amet fermentum.</p>
+
+        <div class="wrapper bg-white animated slideInUp">
+            <div class="container marketing center">
+                <!-- Three columns of text below the carousel -->
+                <div class="row center">
+                    <div class="col-xs-12 below-header">
+                        <p>Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit. Cras mattis consectetur purus sit amet fermentum. Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit. Cras mattis consectetur purus sit amet fermentum.</p>
+                    </div>
                 </div>
 
-                <div class="col-xs-4">
-                    <img class="img-circle" src="data:image/gif;base64,R0lGODlhAQABAIAAAHd3dwAAACH5BAAAAAAALAAAAAABAAEAAAICRAEAOw==" alt="Generic placeholder image" width="140" height="140">
-                    <h2>Heading</h2>
-                    <p>Donec sed odio dui. Etiam porta sem malesuada magna mollis euismod. Nullam id dolor id nibh ultricies vehicula ut id elit.</p>
-                </div><!-- /.col-lg-4 -->
-                <div class="col-xs-4">
-                    <img class="img-circle" src="data:image/gif;base64,R0lGODlhAQABAIAAAHd3dwAAACH5BAAAAAAALAAAAAABAAEAAAICRAEAOw==" alt="Generic placeholder image" width="140" height="140">
-                    <h2>Heading</h2>
-                    <p>Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit. Cras mattis consectetur purus sit amet fermentum.</p>
-                </div><!-- /.col-lg-4 -->
-                <div class="col-xs-4">
-                    <img class="img-circle" src="data:image/gif;base64,R0lGODlhAQABAIAAAHd3dwAAACH5BAAAAAAALAAAAAABAAEAAAICRAEAOw==" alt="Generic placeholder image" width="140" height="140">
-                    <h2>Heading</h2>
-                    <p>Donec sed odio dui. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Vestibulum id ligula porta felis euismod semper.</p>
-                </div><!-- /.col-lg-4 -->
-                <div class="col-xs-4">
-                    <img class="img-circle" src="data:image/gif;base64,R0lGODlhAQABAIAAAHd3dwAAACH5BAAAAAAALAAAAAABAAEAAAICRAEAOw==" alt="Generic placeholder image" width="140" height="140">
-                    <h2>Heading</h2>
-                    <p>Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit. Cras mattis consectetur purus sit amet fermentum.</p>
-                </div><!-- /.col-lg-4 -->
-                <div class="col-xs-4">
-                    <img class="img-circle" src="data:image/gif;base64,R0lGODlhAQABAIAAAHd3dwAAACH5BAAAAAAALAAAAAABAAEAAAICRAEAOw==" alt="Generic placeholder image" width="140" height="140">
-                    <h2>Heading</h2>
-                    <p>Donec sed odio dui. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Vestibulum id ligula porta felis euismod semper.</p>
-                </div><!-- /.col-lg-4 -->
-            </div><!-- /.row -->
+                <div class="row center">
+                    <div class="gallery js-flickity"
+                         data-flickity-options='{ "freeScroll": false, "wrapAround": true }'>
+                        <div class="col-xs-12">
+                            <img class="img-responsive" src="/img/rboptions-org/ico-1.png" alt="">
+                            <h2 class="capitalize">@ln(experience)</h2>
+                            <p>Donec sed odio dui. Etiam porta sem malesuada magna mollis euismod. Nullam id dolor id nibh ultricies vehicula ut id elit.</p>
+                        </div><!-- /.col-lg-4 -->
+                        <div class="col-xs-12">
+                            <img class="img-responsive" src="/img/rboptions-org/ico-2.png" alt="">
+                            <h2 class="capitalize">@ln(investment)</h2>
+                            <p>Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit. Cras mattis consectetur purus sit amet fermentum.</p>
+                        </div><!-- /.col-lg-4 -->
+                        <div class="col-xs-12">
+                            <img class="img-responsive" src="/img/rboptions-org/ico-3.png" alt="">
+                            <h2 class="capitalize">@ln(education)</h2>
+                            <p>Donec sed odio dui. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Vestibulum id ligula porta felis euismod semper.</p>
+                        </div><!-- /.col-lg-4 -->
+                        <div class="col-xs-12">
+                            <img class="img-responsive" src="/img/rboptions-org/ico-4.png" alt="">
+                            <h2 class="capitalize">@ln(customer service)</h2>
+                            <p>Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit. Cras mattis consectetur purus sit amet fermentum.</p>
+                        </div>
+                        <div class="col-xs-12">
+                            <img class="img-responsive" src="/img/rboptions-org/ico-5.png" alt="">
+                            <h2 class="capitalize">@ln(security)</h2>
+                            <p>Donec sed odio dui. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Vestibulum id ligula porta felis euismod semper.</p>
+                        </div><!-- /.col-lg-4 -->
+                    </div>
 
+
+
+
+                    <div class="rubricks hidden-xs">
+                        <div class="col-xs-12 col-sm-4">
+                            <img class="img-responsive" src="/img/rboptions-org/ico-1.png" alt="">
+                            <h2 class="capitalize">@ln(experience)</h2>
+                            <p>Donec sed odio dui. Etiam porta sem malesuada magna mollis euismod. Nullam id dolor id nibh ultricies vehicula ut id elit.</p>
+                        </div><!-- /.col-lg-4 -->
+                        <div class="col-xs-12 col-sm-4">
+                            <img class="img-responsive" src="/img/rboptions-org/ico-2.png" alt="">
+                            <h2 class="capitalize">@ln(investment)</h2>
+                            <p>Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit. Cras mattis consectetur purus sit amet fermentum.</p>
+                        </div><!-- /.col-lg-4 -->
+                        <div class="col-xs-12 col-sm-4">
+                            <img class="img-responsive" src="/img/rboptions-org/ico-3.png" alt="">
+                            <h2 class="capitalize">@ln(education)</h2>
+                            <p>Donec sed odio dui. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Vestibulum id ligula porta felis euismod semper.</p>
+                        </div><!-- /.col-lg-4 -->
+                        <div class="col-sm-4 col-sm-offset-2">
+                            <img class="img-responsive" src="/img/rboptions-org/ico-4.png" alt="">
+                            <h2 class="capitalize">@ln(customer service)</h2>
+                            <p>Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit. Cras mattis consectetur purus sit amet fermentum.</p>
+                        </div>
+                        <div class="col-sm-4">
+                            <img class="img-responsive" src="/img/rboptions-org/ico-5.png" alt="">
+                            <h2 class="capitalize">@ln(security)</h2>
+                            <p>Donec sed odio dui. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Vestibulum id ligula porta felis euismod semper.</p>
+                        </div><!-- /.col-lg-4 -->
+                    </div>
+                </div><!-- /.row -->
+            </div>
+        </div>
+    </div>
+    <div class="wrapper body">
+        <div class="container marketing body center">
             <!-- START THE FEATURETTES -->
-            <hr class="featurette-divider">
-
             <div class="row featurette">
                 <div class="col-md-7">
                     <h2 class="featurette-heading">First featurette heading. <span class="text-muted">It'll blow your mind.</span></h2>
                     <p class="lead">Donec ullamcorper nulla non metus auctor fringilla. Vestibulum id ligula porta felis euismod semper. Praesent commodo cursus magna, vel scelerisque nisl consectetur. Fusce dapibus, tellus ac cursus commodo.</p>
                 </div>
                 <div class="col-md-5">
-                    <img class="featurette-image img-responsive center-block" data-src="holder.js/500x500/auto" alt="Generic placeholder image">
+                    <img class="featurette-image img-responsive center-block" data-src="holder.js/500x500/auto" alt="">
                 </div>
             </div>
 
@@ -110,20 +157,18 @@
                     <img class="featurette-image img-responsive center-block" data-src="holder.js/500x500/auto" alt="Generic placeholder image">
                 </div>
             </div>
-
-            <hr class="featurette-divider">
-
             <!-- /END THE FEATURETTES -->
-
-
-            <!-- FOOTER -->
-            <footer>
-                <p class="pull-right"><a href="#">Back to top</a></p>
-                <p>&copy; 2015 Company, Inc. &middot; <a href="#">Privacy</a> &middot; <a href="#">Terms</a></p>
-            </footer>
-
         </div>
     </div>
+
+    <div class="container">
+        <!-- FOOTER -->
+        <footer>
+            <p class="pull-right"><a href="#">Back to top</a></p>
+            <p>&copy; 2015 Company, Inc. &middot; <a href="#">Privacy</a> &middot; <a href="#">Terms</a></p>
+        </footer>
+    </div>
+
 
 @endsection
 
