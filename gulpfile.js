68737203cda22198c55262bd8462bda16cabbb90
rotemg <rotemg@rboptions.com>
2016-03-10 11:43:38 +0000

    added some structure files

--- gulpfile.js
+++ gulpfile.js
@@ -15,9 +15,12 @@ elixir.config.sourcemaps = true;
 
 elixir(function(mix) {
 
-    /*mix.less(['admin.less'], 'public_html/adminpanel/css/admin.css')
-        */mix.less(['load-ltr.less'], 'public_html/css/bootstrap/rboptions-org-0.css')
-             .less(['load-rtl.less'], 'public_html/css/bootstrap/rboptions-org-1.css');
+    /*mix.less(['admin.less'], 'public_html/adminpanel/css/admin.css') */
+    /*mix.less(['load-ltr.less'], 'public_html/css/bootstrap/rboptions-org-0.css')
+        .less(['load-rtl.less'], 'public_html/css/bootstrap/rboptions-org-1.css');*/
+
+    mix.less(['base-funnel-load-ltr.less'], 'public_html/css/lp/binary-currencies-0.css')
+        .less(['base-funnel-load-rtl.less'], 'public_html/css/lp/binary-currencies-1.css');
 
     //mix.less(['brand/pages/trade.less'], 'public/css/trade.css');
     //mix.less(['brand/pages/accounts.less'], 'public/css/accounts.css');
