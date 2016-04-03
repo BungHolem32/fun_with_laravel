aa2199b40843737f8aad7f4a50b35de286ba6c70
ilanv <ilanv@rboptions.com>
2016-04-03 19:59:03 +0300

    finish mobile

--- resources/views/funnels/layouts/page-layout/aussie-new.blade.php
+++ resources/views/funnels/layouts/page-layout/aussie-new.blade.php
@@ -59,10 +59,10 @@
                 <section id="video_n_form" class="container">
 
                     <div class="row">
-                    @include('funnels.layouts._partials._video',  ['w'=>'100%','h'=>'100%'])
 
                                 <!--Video Tag-->
                         <div id="video_part" class='col-md-7  {{--col-sm-12--}} pull-left'>
+                            @include('funnels.layouts._partials._video',  ['w'=>'100%','h'=>'100%'])
                         </div>
 
                         <!--Form Tag-->
