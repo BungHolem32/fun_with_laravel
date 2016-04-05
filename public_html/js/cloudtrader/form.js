1b4e971d32110ec12131d179531579f4a78e5be9
Or Ben Dahan <orb@rboptions.local>
2016-04-05 15:38:42 +0300

    form was fixed

--- public_html/js/cloudtrader/form.js
+++ public_html/js/cloudtrader/form.js
@@ -3,7 +3,7 @@ $(document).ready(function() {
 	$('#LastName').removeClass().addClass('w-input members-field').parent().removeClass().addClass('w-col w-col-6 w-clearfix');
 	$('#email').removeClass().addClass('w-input members-field').parent().removeClass();
 	$('#password').removeClass().addClass('w-input members-field').parent().removeClass();
-	$('#prefix').removeClass().addClass('w-input members-field w-col').css({'width': '24%'}).parent().removeClass();
+	$('#prefix').removeClass().addClass('w-input members-field w-col').css({'width': '24%', 'padding': '8px 12px'}).parent().removeClass();
 	$('#phone').removeClass().addClass('w-input members-field w-col').css({'width': '75%', 'float': 'right', 'padding': '8px 12px'}).parent().removeClass();
 	$('#submit').removeClass().addClass('w-button members-submit').parent().removeClass();
 });
