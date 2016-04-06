1026f6fede51312bf6c362e85b9c1da050969c0d
ilanv <ilanv@rboptions.com>
2016-04-06 12:25:20 +0300

    aussie_de_funnel creation

--- resources/views/funnels/layouts/page-layout/aussie-ca-members-new.blade.php
+++ resources/views/funnels/layouts/page-layout/aussie-ca-members-new.blade.php
@@ -372,22 +372,6 @@
                     </div>
                 </section>
 
-                <!--Video How_it_work PIC-->
-                <section id="video-how-method-works" class="container">
-                    <div class="row">
-                        <header class="col-md-12 col-sm-12 col-xs-12">
-                            <h2 class="title-question">How The Aussie Method Works</h2>
-                        </header>
-
-                        <div class="video-n-images col-md-12">
-                            <img src="/img/aussie-new/ca/ipad.png" alt="ipad image" class="img-responsive center-block">
-                            <div class="video-tag">
-                                @include('funnels.layouts._partials._video', ['video_url'=>'http://video.chaki.netdna-cdn.com/guide4f.mp4','w'=>'100%', 'h'=>'100%','autoplay'=>'false'])
-                            </div>
-                        </div>
-                    </div>
-                </section>
-
                 {{--LIVE THIRD PARTY TRADE--}}
                 <section id="live-third-party" class="container">
                     <div class="row">
