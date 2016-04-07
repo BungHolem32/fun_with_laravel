5873de5db39d42f2e575b88b583eb86e34c9fcf2
ilanv <ilanv@rboptions.com>
2016-04-07 10:33:05 +0300

    build template for the black panel

--- resources/views/panels/panel-layout/aussie-panel-black.blade.php
+++ resources/views/panels/panel-layout/aussie-panel-black.blade.php
@@ -1,5 +1,5 @@
 @section('head')
-    <link rel="stylesheet" href="/css/panels/black/style.css" />
+    <link rel="stylesheet" href="/css/panels/black/style.css"/>
 @append
 
 @section('bottom-scripts')
@@ -10,8 +10,15 @@
 
 @section('page-layout')
     <div class="container-fluid content-wrapper">
-        <aside class="container account-de-">
-
+        <aside class="container account-details">
+            <div class="row">
+                <header class="col-md-4">
+                    <div class="img-human col-lg-1 col-md-1 col-sm-1 col-xs-1">
+                        <img src="" alt="" class="img-responsive block-center">
+                    </div>
+                    <div class="title"></div>
+                </header>
+            </div>
         </aside>
 
         <header>
