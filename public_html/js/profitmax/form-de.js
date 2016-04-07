1deb22dcc35a98598c5a6365e08404d36de9e2c1
Or Ben Dahan <orb@rboptions.local>
2016-04-07 16:38:59 +0300

    title was fixed in mobile

--- public_html/js/profitmax/form-de.js
+++ public_html/js/profitmax/form-de.js
@@ -7,5 +7,5 @@ $(document).ready(function () {
     $('#password').attr({'placeholder': 'Passwort wählen'}).parent().removeClass().addClass('form-group col-xs-12');
     $('#prefix').attr({'placeholder': 'Vorwahl'}).css({ 'width': '23%' }).removeClass().addClass('form-control area-code col-xs-4 area-code-group').parent().removeClass().addClass('form-group col-xs-12');
     $('#phone').attr({'placeholder': 'Telefonnummer'}).css({ 'float': 'right', 'width': '75%' }).removeClass().addClass('form-control col-xs-8 phone-group');
-    $('#submit').attr({'value': 'KOMPLETE REGISTRIERUNG'}).removeClass().addClass('btn btn-sm register-btn').parent().removeClass().addClass('form-group col-md-12 submit-button-group');
+    $('#submit').attr({'value': 'KOMPLETE REGISTRIERUNG'}).removeClass().addClass('btn btn-sm register-btn').parent().removeClass().addClass('form-group col-xs-12 submit-button-group');
 });
