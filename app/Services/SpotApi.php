685278e32377f73f26431134ed9c40afb1ea6817
rotemg <rotemg@rboptions.com>
2016-05-09 13:24:48 +0000

    testing API Panel

--- app/Services/SpotApi.php
+++ app/Services/SpotApi.php
@@ -14,7 +14,8 @@ class SpotApi
     const ourApiUsername = 'Sitev2';
     const ourApiPassword = '56c09fc848049';
     //const API_URL = "http://api-spotplatform.rboptions.com/api";
-    const API_URL = "http://api-v2.rboptions.com/api";
+    //const API_URL = "http://api-v2.rboptions.com/api";
+    const API_URL = "http://api.rboptions.com/api";
 
     const TIMEOUT = 60;
 
