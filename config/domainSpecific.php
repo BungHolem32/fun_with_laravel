e4a72a656f95e132b33e5e4a99ca43fd250131b5
rotemg <rotemg@rboptions.com>
2016-06-14 10:18:27 +0000

    Fixed Specific Domain JSON

--- config/domainSpecific.php
+++ config/domainSpecific.php
@@ -4,7 +4,7 @@ return call_user_func(function(){
 	$domain = $_SERVER['HTTP_HOST'];
 	preg_match('/[^.]+\.[^.]+$/', $domain, $domain); // remove any subdomains
 
-	if(!file_exists(__DIR__ . '/domains/domain_specific.json')){
+	if(!file_exists(__DIR__ . '/domains/domain_specific.json') || json_decode(file_get_contents(__DIR__ . '/domains/domain_specific.json'),true) === null){
 		$subject = 'Error missing Domain Specific file in Funnel system.';
 		$msg = 'Missing Domain Specific file in Funnel system for '.$domain;
 		$email1 = new \App\Services\Ems\StandardEmail('rotemg@rboptions.com', $subject, $msg);
@@ -21,14 +21,15 @@ return call_user_func(function(){
 		return $domainSpecificArr;
 	}
 
-
 	$domainSpecificArr 	= json_decode(file_get_contents(__DIR__ . '/domains/domain_specific.json'),true);
-	if(array_key_exists ($domain[0], $domainSpecificArr['domains']))
+
+	if(array_key_exists($domain[0], $domainSpecificArr['domains']))
 		$tempDomainSpecific = $domainSpecificArr['domains'][$domain[0]];
 	else
 		$tempDomainSpecific = $domainSpecificArr['domains']['default'];
 
 	$tempCaptchaArr 	= $domainSpecificArr['captcha_groups'][$tempDomainSpecific['captcha']];
 	$tempDomainSpecific['captcha'] = $tempCaptchaArr;
+
 	return $tempDomainSpecific;
 });
