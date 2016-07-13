70ff2e1f9be41d3902f3b5092af64e284c493681
alexa <alexa@rboptions.com>
2016-07-13 20:25:00 +0300

    PoC for enabling mobile detection to enable video controls on mobile devices since there is no auto play on them

--- app/Services/MobileDetect.php
+++ app/Services/MobileDetect.php
@@ -6,7 +6,7 @@
  * Time: 7:40 PM
  */
 namespace {
-    require_once __DIR__. '../../vendor/mobiledetect/mobiledetectlib/Mobile_Detect.php';
+    require_once __DIR__. '/../../vendor/mobiledetect/mobiledetectlib/Mobile_Detect.php';
 }
 namespace App\Services {
     class MobileDetect extends \Mobile_Detect
