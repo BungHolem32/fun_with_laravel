cd7be3eb8c8e8d061a4f02dc7f30529761b3b5a7
ronenk <ronenk@rboptions.com>
2016-08-01 10:21:59 +0300

    Added mix panel checkbox to redirect controller

--- resources/views/main/redirect/edit.blade.php
+++ resources/views/main/redirect/edit.blade.php
@@ -30,5 +30,9 @@
             {!! Form::checkbox('mongo[switches][showCaptcha]', $page['switches']['showCaptcha']) !!}
             {!! Form::label('showCaptcha', 'Show Captcha Always') !!}
         </div>
+        <div class="form-group">
+            {!! Form::checkbox('mongo[switches][mixPanel]', $page['switches']['mixPanel']) !!}
+            {!! Form::label('mixPanel', 'Add Leads To MixPanel') !!}
+        </div>
     </div>
 @stop
